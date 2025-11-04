e44245dd0c937a9c96ccef8ab19ef54404717c93
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-11-04 19:36:44 -0300

    [raw] 🗃️: change messages

--- add_files.sh
+++ add_files.sh
@@ -23,21 +23,25 @@ fi
 changed_files=$(git status -s | awk '{print $2}')
 
 if [ -z "$changed_files" ]; then
-  echo -e "N"
+  echo -e "😎 Repositório 100% atualizado — bora tomar um café?"
   exit 0
 fi
 
 # get selected files
-selected=$(echo "$changed_files" | fzf --bind space:select -m --prompt="Use the spacebar to select the files.: " --preview 'git diff --color=always {}' --border --height=50%)
+selected=$(echo "$changed_files" | fzf --bind space:select -m --prompt="Use a tecla de espaço para selecionar os arquivos.: " --preview 'git diff --color=always {}' --border --height=50%)
 
 # check have a selected files
 if [ -z "$selected" ]; then
-  echo "No files selected."
+  echo "😅 Nenhum arquivo marcado… acho que o commit ficou pra depois do café!"
   exit 0
 fi
 
 # add selected files
 for arq in $selected; do
   git add "$arq"
-  echo "✅ File added: $arq"
+  echo "✅ Arquivo adicionado: $arq"
 done
+
+echo "🌟 Mudanças salvas com sucesso — bora carimbar esse commit?"
+echo
+echo "Digite 'commit' escolher o tipo de commit e a mensagem."
